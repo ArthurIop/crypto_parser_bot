d7ff6e4c904e58c182867a3f4751a0af6edbe077
ArthurIop <70262784+ArthurIop@users.noreply.github.com>
2020-12-17 13:28:34 +0300

    add some checks

--- bot.py
+++ bot.py
@@ -4,7 +4,15 @@ import crypto_parser_bot
 
 token = '1437801621:AAGqirw-CHQ9ecKw2T17pn1QWEj7ANvXrEM'
 bot = telebot.TeleBot(token)
-lol = str(crypto_parser_bot.parse('Kusama'))
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, я крипто-бот, знаю курсы всех известных криптовалют. Могу тебе показать. Введи название любой криптовалюты... ')
+
+@bot.message_handler(commands=['help'])
+def welcome_help(message):
+    bot.send_message(message.chat.id, 'Вводите название с большой буквы. Проверяйте правильность написания, бот пока что чувствителен к регистру')
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
@@ -14,8 +22,5 @@ def get_text_messages(message):
     bot.send_message(message.chat.id, lol)
 
 
-@bot.message_handler(commands=['help'])
-def welcome_help(message):
-    bot.send_message(message.chat.id, 'Чем я могу тебе помочь')
 
 bot.polling()
